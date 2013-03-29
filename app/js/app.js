5364f3c98aa2caca9c99f157ad88114caa388767
Mihai Nicolae <m.nicolae91@gmail.com>
2013-03-29 00:09:32 -0400

    add some bad-ass jquery

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 	
+	$(".img-polaroid").hover(function() {
+
+	});
+
+	$(".img-polaroid").click(function() { 
+		$('input#username_field').show();
+		$('input#password_field').show();
+		$('button#login_button').show();
+	});
+
 });
