48f390f93e89390ee23142a64f8597dd15c071e9
Mihai Nicolae <m.nicolae91@gmail.com>
2013-03-29 01:19:44 -0400

    calling it a night

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	
 	$(".img-polaroid").hover(
-		function() { $(this).css("width", "100"); $(this).css("height", "100"); },
-		function() { $(this).css("width", "60"); $(this).css("height", "60");}
+		function() { $(this).css("width", "140"); $(this).css("height", "140"); },
+		function() { $(this).css("width", "100"); $(this).css("height", "100");}
 	);
 
-	$(".img-polaroid").click(function() { 
+	$(".img-polaroid").click(function() {
 		$('input#username_field').show();
 		$('input#password_field').show();
 		$('button#login_button').show();
