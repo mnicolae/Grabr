68f1ec3ec5c5487d7608ccfd069c76d9b4934f4b
Adir <adirkrafman@gmail.com>
2013-03-30 15:23:18 -0400

    Front Page work

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,45 @@
 $(document).ready(function() {
+
+	var selected = false;
 	
-	$(".img-polaroid").hover(
-		function() { $(this).css("width", "140"); $(this).css("height", "140"); },
-		function() { $(this).css("width", "100"); $(this).css("height", "100");}
+	$(".img-polaroid").hover( 
+
+		function(){
+		if (!selected) {
+			$(this).css("width", "110"); $(this).css("height", "110");
+			// $(this).css("width", "100"); $(this).css("height", "100");
+			}
+		},
+
+		function(){
+		if (!selected) {
+			// $(this).css("width", "110"); $(this).css("height", "110");
+			$(this).css("width", "100"); $(this).css("height", "100");
+			}
+		}
 	);
 
 	$(".img-polaroid").click(function() {
-		$('input#username_field').show();
-		$('input#password_field').show();
-		$('button#login_button').show();
+		console.log(selected);
+		if (selected) {
+			$(".img-polaroid").css("width", "100"); $(".img-polaroid").css("height", "100");
+			$('#password_field').hide(); $('#username_field').hide();
+			$('button#login_button').hide(); $('#forgot').hide();
+			selected = false;
+		}
+		else{
+			// $('input#username_field').show();
+			$(this).css("width", "110"); $(this).css("height", "110");
+			$('#password_field').show();
+			$('button#login_button').show(); $('#forgot').show();
+			selected = true;
+		}
+	});
+
+	$(".img-other").click(function() {
+		$('#username_field').show();
+		$('#password_field').show();
+		$('button#login_button').show(); $('#forgot').show();
 	});
 
 });
